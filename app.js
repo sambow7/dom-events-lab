75082325a958f1718aac4c9a9dd39e5c1474061c
Sam Bowen <samhbowen@gmail.com>
2024-10-22 09:42:50 -0700

    fresh  start

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-const buttons = document.querySelectorAll('.button');
-const calculator = document.querySelector('#calculator');
+const buttons = document.querySelectorAll(".button");
+const calculator = document.querySelector("#calculator");
+const display = document.querySelector(".display");
 /*-------------------------------- Variables --------------------------------*/
-let displpayValue = "";
+let displayValue = "";
 let inputOperator = "";
-let inputNumber1  = "";
-let inputNumber2  = "";
+let inputNumber1 = "";
+let inputNumber2 = "";
 let clearCalculator = "";
-let resultCalulator = "";
+let calculatorTotal = 0;
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -16,26 +17,48 @@ let resultCalulator = "";
 /*-------------------------------- Functions --------------------------------*/
 
 buttons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    // This log is for testing purposes to verify we're getting the correct value
-    // console.log(event.target.innerText);
-    // Future logic to capture the button's value would go here...
-  });
-});
-
-
-calculator.addEventListener('click', (event) => {
+  button.addEventListener('click', pressButtonclick)
   // This log is for testing purposes to verify we're getting the correct value
-  // You have to click a button to see this log  
-  console.log(event.target.innerText);
+  // console.log(event.target.innerText);
+  // Future logic to capture the button's value would go here...
 
-  if (event.target.classList.contains('number')) {
-    return parseFloat( inputNumber1 + nummber);
-  }
-
-  // Example
-  if (event.target.innerText === '*') {
-    // Do something with this operator
+  function pressButtonclick(event) {
+    const buttonClicked = event.target.innerText;
+    if (!isNaN(parseFloat(buttonClicked))) {
+      displayValue += buttonClicked;~
+      display.innerText += displayValue;
+    } else if (buttonClicked) {
+      switch (buttonClicked) {
+        case "+":
+          return inputNumber1 + inputNumber2;
+          break;
+        case "-":
+          return inputNumber1 - inputNumber2;
+          break;
+        case "*":
+          return inputNumber1 * inputNumber2;
+          break;
+        case "/":
+          return inputNumber1 / inputNumber2;
+          break;
+        case "=":
+          return sumFunction;
+          break;
+        case "C":
+          return displayValue = "";
+          break;
+      }
+    }
   }
 });
 
+function sumFunction(sum) {
+  let result = -1;
+  let i;
+  for (i = 1; i < arguments.length; i++) {
+    result += arguments[i] + sum;
+  }
+  return result;
+}
+
+console.log(sumFunction("====== SUM of numbers ===="1,2,5))
